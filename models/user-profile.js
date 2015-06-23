383e4f893953209295f743cd210cba565b3f5782
Jorge Guiñez <neo_yor@hotmail.com>
2015-06-23 16:32:50 -0300

    Revert "más cambios pequeños"
    This reverts commit c60dc040abd2d5a7abf4943cf5cdd430f2413338.

--- models/user-profile.js
+++ models/user-profile.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var UserProfileModel = function(cnf) {
-    this.email = cnf.email,
-    this.firstName = cnf.firstName,
-    this.lastName = cnf.lastName
+    this.email = cnf.email;
+    this.firstName = cnf.firstName;
+    this.lastName = cnf.lastName;
 };
 
 module.exports = UserProfileModel;
